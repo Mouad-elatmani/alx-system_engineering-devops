e2a99b41b17475d0737517e4911059b2cea19bee
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-26 11:49:12 -0800

    Create File

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #create a file in /tmp
 file { '/tmp/school':
-  ensure => file,
-  content => "I love Puppet\n",
-  group => 'www-data'
-  mode => '0774',
-  owner => 'www-data',
+  ensure  => present ,
+  content => 'I love Puppet\n',
+  group   => 'www-data'
+  mode    => '774',
+  owner   => 'www-data',
 }
